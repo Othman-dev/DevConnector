46f28ac653c386fb89483a2de6d2f37888912f5e
Othmay <othmay@gmail.com>
2020-10-14 15:24:08 +0200

    section 2: Express & MongoDB Setup

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const app = require('express')();
+const connectDB= require('./config/db');
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 //PORT & listen
 const PORT = process.env.port || 5000;
