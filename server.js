1978506cb65d63f83d1622db14bb4c3a022f8efb
Othmay <othmay@gmail.com>
2020-10-16 14:22:27 +0200

    section 4: Profile and Api Routes

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const app = require('express')();
-const connectDB= require('./config/db');
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
 
 // Connect Database
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false}));
 
 app.get('/', (req, res) => res.send('API Running'));
 
